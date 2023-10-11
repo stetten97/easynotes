c7c97d68d59fa810e145093c03fe5bc252a9662f
Moritz von Stetten <moritzvonstetten97@gmail.com>
2023-10-11 23:54:26 +0200

    Add code for blueprints and migrations

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
 from models import db
+from views.notes import bp
 
 app = Flask(__name__)
+app.register_blueprint(bp)
+
+# app.register_blueprint(bp)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.db"
+
+migrate = Migrate(app, db)
 
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
-
-@app.route("/")
-def hello_word():
-    return '<h1>hello world</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
