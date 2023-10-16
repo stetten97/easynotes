7f6c40cd588be478b0699cd79c7ab610bfe198ee
Moritz von Stetten <moritzvonstetten97@gmail.com>
2023-10-16 20:54:51 +0200

    Fix typo and make Note title nullable

--- models.py
+++ models.py
@@ -9,14 +9,14 @@ class Notebook(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    notes = db.relationship('Notes', backref='notebook', lazy=True)
+    notes = db.relationship('Note', backref='notebook', lazy=True)
 
 class Note(db.Model):
     """
     One note in a notebook.
     """
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique=False, nullable=False)
+    title = db.Column(db.String(100), unique=False, nullable=True)
     content = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
